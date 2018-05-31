7e634801f656700ddc83ad5552d6d5672766d375
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-05-30 22:00:02 -0500

    added an image, recipe name, and description using the recipe model.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+   recipes: Recipe[] = [
+      // creating a new Recipe by instantiating the Recipe class and using the 'new' keyword.
+      new Recipe ('Lefse','A soft grilled thinly rolled tortilla made with potatoes','http://d1dd4ethwnlwo2.cloudfront.net/wp-content/uploads/2016/05/lefse-main.jpg') 
+   ];
+   constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+   ngOnInit() {
+   
+   }
 
 }
