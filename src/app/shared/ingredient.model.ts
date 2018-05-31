a3203a414c0926da39206077f0c156deb39051d3
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-05-30 23:49:02 -0500

    added an ingredient model in a shared folder, and added to the ingredients array dynamically to put the ingredients in the shopping list.

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+// this defines how ingredients should look; the ingredient model. 
+export class Ingredient {
+   constructor (public name: string, public amount: number) {}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+// imports the ingredient class that was created in the shared folder as the Ingredient model.
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
+
 export class ShoppingListComponent implements OnInit {
+   ingredients: Ingredient[] = [
+      // instantiates a new Ingredient class via the construtor. 
+      new Ingredient ('potatoes', 25),
+      new Ingredient ('flour', 10)
+   ];
+   
+   constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
+   ngOnInit() {
   }
 
 }
