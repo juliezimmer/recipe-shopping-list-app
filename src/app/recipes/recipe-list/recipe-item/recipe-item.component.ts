db326ac95d3cbe7e35bb8ba35ff4691b9fd852e5
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-06-02 16:19:15 -0500

    can now select a recipe from the list of recipes and see the details view of it.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -11,9 +11,14 @@ export class RecipeItemComponent implements OnInit {
    // there won't be a value assined ro recipe because it comes from outside this component. This also requires the @Input Decorator. @Input allows this component property (recipe) from outside the component.
    @Input() recipe: Recipe;
    
+   // new Event Emitted
+   @Output() recipeSelected = new EventEmitter <void> ();   
    constructor() { }
 
    ngOnInit() {
    }
 
+   onSelected(){
+      this.recipeSelected.emit();
+   }
 }
