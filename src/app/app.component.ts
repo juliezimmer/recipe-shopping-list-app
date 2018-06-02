5a006eb74501e65fb29759945133af5811ab94be
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-06-02 13:29:11 -0500

    added functionality to navigation links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   loadedLink = 'recipe';
+   
+   // from app template
+   onNavigate(link: string){
+      this.loadedLink = link;
+
+   }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 // The Component feature of Angular has to be imported from @angular/core
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 // This HeaderComponent class will not be recognized as a component by Angular without the @Component() decortor.
 @Component({
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
    templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+   // event emitted when link is selected
+   // this creates an event emitter based on the event emitter class.
+   @Output () linkSelected = new EventEmitter <string> ();
+   
+   onSelect(link: string){
+      //using the property that holds the event to emit an event whenever one of the links in the nav bar is clicked.
+      this.linkSelected.emit(link);
 
+   }
 }
