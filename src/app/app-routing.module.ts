820a6e702fd7dfbe5ad9c8c1a056be5c2c70d376
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-06-27 13:16:02 -0500

    added two child routes to  the RecipeComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
    // the path that is reached when the app is loaded for the first time
@@ -11,8 +13,10 @@ const appRoutes: Routes = [
       path: 'recipes', 
       component: RecipesComponent,
       children: [
-         { path:'', component: }
-      ] },
+         { path:'', component: RecipeStartComponent },
+         { path: ':id', component: RecipeDetailComponent }
+      ] 
+   },
    { path: 'shopping-list', component: ShoppingListComponent }
 ];
 @NgModule({
